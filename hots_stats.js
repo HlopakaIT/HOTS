90d13c7a3a0d5689121778a64f233191929c6f1c
HlopakA <faka2@ukr.net>
2024-11-24 13:10:45 +0300

    two_pages_split

--- hots_stats.js
+++ hots_stats.js
@@ -40,42 +40,6 @@ function showStats(event) {
     statsBlock.style.display = "table"
 }
 
-
-const recordButton = document.getElementById("add-record-button")
-
-recordButton.addEventListener("click", addRecord)
-
-const winLoseRadio = document.getElementsByName("win-lose")
-
-function addRecord(event) {
-    const heroTypeArea = document.getElementById("hero-type-area")
-    for(let i = 0; i < winLoseRadio.length; i++) {
-        if(winLoseRadio[i].checked) {
-            const winLoseValue = winLoseRadio[i].value
-            const gameScore = {
-                hero: heroTypeArea.value,
-                win: switchScoreIntoTrueFalse(winLoseValue)
-            }
-
-            heroesStats.push(gameScore)
-
-            localStorage.setItem(
-                "userMatchHistory",
-                JSON.stringify(
-                    heroesStats
-                )
-            )
-        }
-    }
-
-}
-
-function switchScoreIntoTrueFalse(winLoseValue) {
-    if(winLoseValue === "win") return true
-    
-    return false
-}
-
 const clearMatchHistoryButton = document.getElementById("clear-match-history-button")
 
 clearMatchHistoryButton.addEventListener("click", clearMatchHstory)
